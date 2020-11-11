c6141f668ebd162b3d8584d749105d3a416fb998
Vladislav Chiorescu <vch@insomniacdesign.com>
2020-11-11 18:14:51 +0200

    Remove unnecessary interface.

--- src/modules/paths/index.ts
+++ src/modules/paths/index.ts
@@ -6,7 +6,6 @@ export const paths = {
     addRouteTemplate: './lib/Templates/Routes.mustache',
     compInterfaceIndex: './lib/Templates/Interfaces/ComponentIndex.mustache',
     createInterfaceTempPath: './lib/Templates/Interfaces/Component.mustache',
-    pageInterfaceIndex: './lib/Templates/Interfaces/PageIndex.mustache',
     classComponentTemplatePath: './lib/Templates/Components/Class.mustache',
     componentIndexTemplatePath: './lib/Templates/Components/index.mustache',
     funcComponentTemplate: './lib/Templates/Components/Functional.mustache',
